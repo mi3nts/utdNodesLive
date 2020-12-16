defdd6f9f1e9f9897593c3edf4da4577d409adbd
lakithaomal <you@example.com>
2020-12-15 18:59:33 -0600

    daily Updates

--- firmware/mainLive.m
+++ firmware/mainLive.m
@@ -7,20 +7,33 @@ close all
 addpath("functions/");
 
 addpath("YAMLMatlab_0.4.3");
-    currentDate= datetime('now','timezone','utc');
-    display(currentDate);
-    
-daysBack = daysact(datetime(2018,1,1),today)    
+
+% currentDate= datetime('now','timezone','utc');
+% display(currentDate);
+%     
+% daysBack = daysact(datetime(2020,10,10),today)    
 % 
- for n = 1:15
-%     try
-        livePlotter(string(n),...
-            '/media/teamlary/teamlary1/gitHubRepos/matlabMCRMints/firmware/liveRunDynamic/mintsDefinitions.yaml'...
-            );
-%     catch e
-%         display(e)     
-%     end    
-end
+
+% liveRunTimedDynamic(string(11),...
+%             '/media/teamlary/teamlary1/gitHubRepos/utdNodesLive/firmware/mintsDefinitions.yaml'...
+%                  );
+
+while (true)
+     for n = 1:15
+        try
+            liveRunTimedDynamic(string(n),...
+                'mintsDefinitions.yaml'...
+                );
+        catch e
+            display(e)     
+        end
+        
+        clear all -except n
+        pause(5)
+    end
+end 
+
+
 
 % 
 % currentDate= datetime('now','timezone','utc');
